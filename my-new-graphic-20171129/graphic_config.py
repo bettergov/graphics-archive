db29c5bc20e390734af147c9b10d6081aa4855e4
Patrick Judge <pjudge@bettergov.org>
2017-11-29 11:19:28 -0600

    Initial commit

---
+++ my-new-graphic-20171129/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1P2ubS0uZ0JShQ8obc7CkA_flWxA7XFyDqOVJ3UknFNY'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
