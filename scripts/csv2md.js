2bc77363b7d9d59b629897633338b0a66d37a73e
Patrick Judge <pjudge@bettergov.org>
2019-08-06 11:40:30 -0500

    Sort contents.md

--- scripts/csv2md.js
+++ scripts/csv2md.js
@@ -7,9 +7,9 @@ var csvToMarkdown = require('csv-to-markdown-table');
 const rootDir = path.dirname(__dirname);
 const csvPath = path.join(rootDir, 'contents.csv');
 const stream = fs.createReadStream(csvPath);
-
 const rows = [];
 
+// parse and transform original csv
 const parser = csv.parse({ headers: true }).transform(row => ({
   localPath: `[${row.localPath}](${row.localPath})`,
   remotePath: `[${row.remotePath}](https://graphics.bettergov.org/${
@@ -25,7 +25,18 @@ stream
   })
   .on('end', () => {
     const fields = ['localPath', 'remotePath', 'embeddedOn'];
-    const parsed = json2csv.parse(rows, { fields, quote: '' });
+    const parsed = json2csv.parse(
+      rows.sort((a, b) => {
+        if (a.localPath < b.localPath) {
+          return -1;
+        }
+        if (a.localPath > b.localPath) {
+          return 1;
+        }
+        return 0;
+      }),
+      { fields, quote: '' }
+    );
     const md = csvToMarkdown(parsed, ',', true);
     const mdPath = path.join(rootDir, 'contents.md');
 
